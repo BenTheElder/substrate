01c92e9da37792b7efc0dd7691d03c807f3ff3bf
Noureldin <105996926+nmn3m@users.noreply.github.com>
2026-08-19 23:47:54 +0300

    Allow actors to opt in/out of Linux capabilities (#795)
    Fixes #744
    - [x] Tests pass
    - [x] Appropriate changes to documentation are included in the PR

--- cmd/atelet/main.go
+++ cmd/atelet/main.go
@@ -1486,6 +1486,7 @@ func (s *AteomHerder) prepareOCIBundles(
 			"", // pause is sandbox infra; it gets no actor identity mount.
 			nil,
 			nil,
+			nil, // pause only reaps; it needs no capabilities.
 		); err != nil {
 			return wrapFileSystemErr("while creating pause OCI bundle", err)
 		}
@@ -1518,6 +1519,7 @@ func (s *AteomHerder) prepareOCIBundles(
 				identityDir,
 				spec.GetVolumes(),
 				ctr.GetVolumeMounts(),
+				resolveCapabilities(ctr.GetSecurityContext().GetCapabilities()),
 			); err != nil {
 				return wrapFileSystemErr(fmt.Sprintf("while creating %q OCI bundle", ctr.GetName()), err)
 			}
